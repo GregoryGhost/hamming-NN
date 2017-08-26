329a80a7a0afbbef4eec800849e80d021c5524f4
Gregory Kulakov <lobster287@gmail.com>
2017-08-26 18:25:12 +0700

    Fixing bugs in MatrixOperation.fsx -algorithm correction func "!--/MatrixDifference", the function does not change the value of the matrix (m1); -algorithm optimization func "t(m)-Transpose"

--- MatrixOperation.fs
+++ MatrixOperation.fs
@@ -18,32 +18,26 @@ let toVector (x : float [,])=
                                             let vj = j-vi*ncol
                                             x.[vi,vj])
     vector
-let private checkSize m1 m2 func =
+let t(m) = 
+    let nrow = Array2D.length1 m
+    let ncol = Array2D.length2 m
+    let res = Array2D.init ncol nrow (fun i j ->
+                                         m.[j,i])
+    res
+let (!--) (m1:float [,], m2:float [,]) =
     let l1m1 = Array2D.length1 m1
     let l2m1 = Array2D.length2 m1
     let l1m2 = Array2D.length1 m2
     let l2m2 = Array2D.length2 m2
     let check = (l1m1=l1m2)&&(l2m1=l2m2)
-    if not check then failwith "diffrent size Array2D"
-    else func
-let t(m) = 
-    let nrow = Array2D.length1 m
-    let ncol = Array2D.length2 m
-    let res = Array2D.zeroCreate ncol nrow
-    let eval = 
-        for i in 0..nrow-1 do
-            for j in 0..ncol-1 do
-                res.[j,i] <- m.[i,j]
-    res
-let (!--) (m1:float [,], m2:float [,]) =
-    let run =
-        let srow=Array2D.length1 m1
-        let scol=Array2D.length2 m1
-        for i=0 to (srow-1) do
-            for j=0 to (scol-1) do
-            m1.[i,j]<-m1.[i,j]-m2.[i,j]
-        m1
-    checkSize m1 m2 run
+    if not check then 
+        failwith "diffrent size Array2D"
+    else
+        let srow = l1m1
+        let scol = l2m1
+        let result = Array2D.init srow scol (fun i j ->
+                                               m1.[i,j]-m2.[i,j])
+        result
 let maxValueToIndexCol (x : float [,]):int=
     let mutable maxX = 0.0
     let mutable index = 0
@@ -57,16 +51,14 @@ let maxValueToIndexCol (x : float [,]):int=
     ) {0..(Array2D.length1 x)-1}
     index
 let (^^*) m1 m2 =
-    let run = 
-        let scol=Array2D.length2 m1
-        let srow=Array2D.length1 m2
-        if scol<>srow then raise <| InvalidColAndRowMatrixException(scol, srow)
-        let res:float [,] = Array2D.zeroCreate (Array2D.length1 m1) (Array2D.length2 m2)
-        let genRes =
-            for i in 0..(Array2D.length1 m1)-1 do
-                for k in 0..(Array2D.length2 m2)-1 do
-                    for j in 0..scol-1 do
-                        res.[i,k] <- res.[i,k] + m1.[i,j] * m2.[j,k]
+    let scol=Array2D.length2 m1
+    let srow=Array2D.length1 m2
+    if scol<>srow then raise <| InvalidColAndRowMatrixException(scol, srow)
+    let res:float [,] = Array2D.zeroCreate (Array2D.length1 m1) (Array2D.length2 m2)
+    let genRes =
+        for i in 0..(Array2D.length1 m1)-1 do
+            for k in 0..(Array2D.length2 m2)-1 do
+                for j in 0..scol-1 do
+                    res.[i,k] <- res.[i,k] + m1.[i,j] * m2.[j,k]
 
-        res
-    run                                    
+    res                              
